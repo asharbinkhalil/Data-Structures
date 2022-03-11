a8564577c638a09bbf7c854cb17e7dd3fe0e30d3
Ashar Khalil <68750622+asharbinkhalil@users.noreply.github.com>
2022-03-11 12:14:23 +0500

    Update sortingalgos.cpp

--- sortingalgos.cpp
+++ sortingalgos.cpp
@@ -1,16 +1,38 @@
 #include<iostream>
 using namespace std;
-void bubblesort(int arr[], int n){
-     for(int i=0; i<n-1; i++)
-        {
-	if(arr[i]>arr[i+1])
+void bubbleSort(int arr[], int n)
+{
+    bool found=false;
+	while(!found)
 	{
-	        int val=arr[i];
-		arr[i]=arr[i+1];
-		arr[i+1]=val;
-		i=-1;
+		found =true;
+    for(int i=0; i<n; i++)
+    {
+       for(int j=0; j<n-i-1; j++)
+       {
+           if(arr[j]>arr[j+1])
+           {
+               int temp;
+               temp=arr[j];
+               arr[j]=arr[j+1];
+               arr[j+1]=temp;
+		   found=false;
+           }
+       }
+    }
 	}
 }
+   void insertionSort(int arr[], int n)
+{
+	
+	for(int i=1; i<n ; i++)
+	{
+		for(int j=i; j>0; j--)
+		{
+			if( (arr[j]<arr[j-1]))
+			swap(&arr[j-1],&arr[j]);
+		}
+	}
 }
 void swap(int *a, int *b )
 {
